2a09f4696bdc1afb76f89191e893d832caaed838
Александр Пекшев <cjmodis@gmail.com>
2022-04-03 21:44:25 +0300

    Accounting for plan rotation

--- mprCopyCropBox/Command.cs
+++ mprCopyCropBox/Command.cs
@@ -64,7 +64,7 @@ public class Command : IExternalCommand
             if (!checkedViews.Any())
                 return Result.Cancelled;
 
-            using (var tr = new Transaction(doc, Language.GetPluginLocalName(ModPlusConnector.Instance)))
+            using (var tr = new TransactionGroup(doc, Language.GetPluginLocalName(ModPlusConnector.Instance)))
             {
                 tr.Start();
 
@@ -78,7 +78,7 @@ public class Command : IExternalCommand
                     CopyCropBox(sourceView, checkedView.View, context.CopyCropVisibilitySettings);
                 }
 
-                tr.Commit();
+                tr.Assimilate();
             }
 
             return Result.Succeeded;
@@ -109,52 +109,121 @@ public class Command : IExternalCommand
             ViewType.Schedule or
             ViewType.ColumnSchedule or
             ViewType.PanelSchedule or
-            ViewType.DrawingSheet);
+            ViewType.DrawingSheet or
+            ViewType.Rendering);
     }
 
     private void CopyCropBox(View sourceView, View targetView, bool copyCropVisibilitySettings)
     {
+        var doc = sourceView.Document;
+
         var cropBoxVisible = targetView.CropBoxVisible;
-        targetView.CropBoxVisible = true;
         var cropBoxActive = targetView.CropBoxActive;
-        targetView.CropBoxActive = true;
         var annotationCropActiveParameter = targetView.get_Parameter(BuiltInParameter.VIEWER_ANNOTATION_CROP_ACTIVE);
         var annotationCropActive = annotationCropActiveParameter.AsInteger();
-        annotationCropActiveParameter.Set(1);
 
-        if (sourceView.ViewType == ViewType.ThreeD && targetView.ViewType == ViewType.ThreeD)
+        using (var tr = new Transaction(doc, "Modify target view crop box"))
         {
-            targetView.CropBox = sourceView.CropBox;
+            tr.Start();
+            targetView.CropBoxVisible = true;
+            targetView.CropBoxActive = true;
+            annotationCropActiveParameter.Set(1);
+            tr.Commit();
         }
-        else
-        {
-            var sourceCropRegionShapeManager = sourceView.GetCropRegionShapeManager();
-            var targetCropRegionShapeManager = targetView.GetCropRegionShapeManager();
 
-            if (sourceCropRegionShapeManager.CanHaveAnnotationCrop && targetCropRegionShapeManager.CanHaveAnnotationCrop)
+        if (sourceView.ViewType.ToString().Contains("Plan"))
+        {
+            var angle = sourceView.RightDirection.AngleTo(XYZ.BasisX);
+
+            if (angle != 0.0)
             {
-                targetCropRegionShapeManager.BottomAnnotationCropOffset = sourceCropRegionShapeManager.BottomAnnotationCropOffset;
-                targetCropRegionShapeManager.LeftAnnotationCropOffset = sourceCropRegionShapeManager.LeftAnnotationCropOffset;
-                targetCropRegionShapeManager.TopAnnotationCropOffset = sourceCropRegionShapeManager.TopAnnotationCropOffset;
-                targetCropRegionShapeManager.RightAnnotationCropOffset = sourceCropRegionShapeManager.RightAnnotationCropOffset;
+                var targetCropBoxElement = GetCropBoxElement(targetView);
+
+                using (var tr = new Transaction(doc, "Rotate crop box element"))
+                {
+                    var bBox = targetView.CropBox;
+                    var center = 0.5 * (bBox.Max + bBox.Min);
+                    var axis = Line.CreateBound(center, center + XYZ.BasisZ);
+
+                    tr.Start();
+                    ElementTransformUtils.RotateElement(doc, targetCropBoxElement.Id, axis, angle);
+                    tr.Commit();
+                }
+            }
+        }
+
+        using (var tr = new Transaction(doc, "Copy extents"))
+        {
+            tr.Start();
+            if (sourceView.ViewType == ViewType.ThreeD && targetView.ViewType == ViewType.ThreeD)
+            {
+                targetView.CropBox = sourceView.CropBox;
+            }
+            else
+            {
+                var sourceCropRegionShapeManager = sourceView.GetCropRegionShapeManager();
+                var targetCropRegionShapeManager = targetView.GetCropRegionShapeManager();
+
+                if (sourceCropRegionShapeManager.CanHaveAnnotationCrop && targetCropRegionShapeManager.CanHaveAnnotationCrop)
+                {
+                    targetCropRegionShapeManager.BottomAnnotationCropOffset = sourceCropRegionShapeManager.BottomAnnotationCropOffset;
+                    targetCropRegionShapeManager.LeftAnnotationCropOffset = sourceCropRegionShapeManager.LeftAnnotationCropOffset;
+                    targetCropRegionShapeManager.TopAnnotationCropOffset = sourceCropRegionShapeManager.TopAnnotationCropOffset;
+                    targetCropRegionShapeManager.RightAnnotationCropOffset = sourceCropRegionShapeManager.RightAnnotationCropOffset;
+                }
+                
+                var cropShape = sourceCropRegionShapeManager.GetCropShape().FirstOrDefault();
+                if (cropShape != null)
+                    targetCropRegionShapeManager.SetCropShape(cropShape);
             }
 
-            var cropShape = sourceCropRegionShapeManager.GetCropShape().FirstOrDefault();
-            if (cropShape != null)
-                targetCropRegionShapeManager.SetCropShape(cropShape);
+            if (copyCropVisibilitySettings)
+            {
+                targetView.CropBoxVisible = sourceView.CropBoxVisible;
+                targetView.CropBoxActive = sourceView.CropBoxActive;
+                annotationCropActiveParameter.Set(sourceView.get_Parameter(BuiltInParameter.VIEWER_ANNOTATION_CROP_ACTIVE).AsInteger());
+            }
+            else
+            {
+                targetView.CropBoxVisible = cropBoxVisible;
+                targetView.CropBoxActive = cropBoxActive;
+                annotationCropActiveParameter.Set(annotationCropActive);
+            }
+
+            tr.Commit();
+        }
+    }
+
+    private Element GetCropBoxElement(View view)
+    {
+        var doc = view.Document;
+        Element cropBoxElement;
+        using (var trGroup = new TransactionGroup(doc, "Temp to find crop box element"))
+        {
+            trGroup.Start();
+
+            using (var tr = new Transaction(doc, "Temp to find crop box element"))
+            {
+                tr.Start();
+                view.CropBoxVisible = false;
+                tr.Commit();
+
+                var collector = new FilteredElementCollector(doc, view.Id);
+
+                var shownElems = collector.ToElementIds();
+
+                tr.Start();
+                view.CropBoxVisible = true;
+                tr.Commit();
+
+                collector = new FilteredElementCollector(doc, view.Id);
+                collector.Excluding(shownElems);
+                cropBoxElement = collector.FirstElement();
+            }
+
+            trGroup.RollBack();
         }
 
-        if (copyCropVisibilitySettings)
-        {
-            targetView.CropBoxVisible = sourceView.CropBoxVisible;
-            targetView.CropBoxActive = sourceView.CropBoxActive;
-            annotationCropActiveParameter.Set(sourceView.get_Parameter(BuiltInParameter.VIEWER_ANNOTATION_CROP_ACTIVE).AsInteger());
-        }
-        else
-        {
-            targetView.CropBoxVisible = cropBoxVisible;
-            targetView.CropBoxActive = cropBoxActive;
-            annotationCropActiveParameter.Set(annotationCropActive);
-        }
+        return cropBoxElement;
     }
 }
